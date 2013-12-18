4fe7acacd6852db5f34aedd84a87e93ead13037f
Sudarshan Sreenivasan <sudarshan89@outlook.com>
2013-12-18 11:29:39 +0800

    Corrections, reverese engineered from class files

--- src/main/java/com/conf/Talk.java
+++ src/main/java/com/conf/Talk.java
@@ -1,69 +1,69 @@
-/*  1:   */ package com.conf;
-/*  2:   */ 
-/*  3:   */ import org.joda.time.Duration;
-/*  4:   */ import org.joda.time.LocalTime;
-/*  5:   */ 
-/*  6:   */ class Talk
-/*  7:   */ {
-/*  8:12 */   static final Duration SHORT_TALK_DURATION = Duration.standardMinutes(5L);
-/*  9:   */   String talkName;
-/* 10:   */   Duration talkDuration;
-/* 11:   */   private LocalTime startsOn;
-/* 12:   */   LocalTime endsOn;
-/* 13:   */   
-/* 14:   */   static Talk normalTalk(String talkName, Duration talkDuration, LocalTime startsOn)
-/* 15:   */   {
-/* 16:20 */     return new Talk(talkName, talkDuration, startsOn);
-/* 17:   */   }
-/* 18:   */   
-/* 19:   */   static Talk shortTalk(String talkName, LocalTime startsOn)
-/* 20:   */   {
-/* 21:24 */     return new Talk(talkName, SHORT_TALK_DURATION, startsOn);
-/* 22:   */   }
-/* 23:   */   
-/* 24:   */   Talk(String talkName, Duration talkDuration, LocalTime startsOn)
-/* 25:   */   {
-/* 26:28 */     this.talkName = talkName;
-/* 27:29 */     this.talkDuration = talkDuration;
-/* 28:30 */     this.startsOn = startsOn;
-/* 29:31 */     this.endsOn = startsOn.plus(talkDuration.toStandardSeconds());
-/* 30:   */   }
-/* 31:   */   
-/* 32:   */   public boolean isTalkOverlapping(LocalTime proposedStartTimeOfTalk, LocalTime proposedEndsTimeOfTalk)
-/* 33:   */   {
-/* 34:35 */     return ((proposedStartTimeOfTalk.isAfter(this.startsOn)) && (proposedStartTimeOfTalk.isBefore(this.endsOn))) || ((proposedStartTimeOfTalk.isBefore(this.startsOn)) && (proposedEndsTimeOfTalk.isAfter(this.startsOn)));
-/* 35:   */   }
-/* 36:   */   
-/* 37:   */   public boolean isShortTalk()
-/* 38:   */   {
-/* 39:40 */     return this.talkDuration.equals(SHORT_TALK_DURATION);
-/* 40:   */   }
-/* 41:   */   
-/* 42:   */   public boolean equals(Object o)
-/* 43:   */   {
-/* 44:45 */     if (this == o) {
-/* 45:45 */       return true;
-/* 46:   */     }
-/* 47:46 */     if ((o == null) || (getClass() != o.getClass())) {
-/* 48:46 */       return false;
-/* 49:   */     }
-/* 50:48 */     Talk talk = (Talk)o;
-/* 51:50 */     if (this.talkName != null ? !this.talkName.equals(talk.talkName) : talk.talkName != null) {
-/* 52:50 */       return false;
-/* 53:   */     }
-/* 54:52 */     return true;
-/* 55:   */   }
-/* 56:   */   
-/* 57:   */   public int hashCode()
-/* 58:   */   {
-/* 59:57 */     return this.talkName != null ? this.talkName.hashCode() : 0;
-/* 60:   */   }
-/* 61:   */   
-/* 62:   */   static boolean isValidNetworkingEventStartTime(LocalTime startsOn)
-/* 63:   */   {
-/* 64:61 */     return (startsOn.isAfter(LocalTime.parse("T16:00:00"))) && (startsOn.isBefore(LocalTime.parse("T17:00:00")));
-/* 65:   */   }
-/* 66:   */ }
+package com.conf;
+
+import org.joda.time.Duration;
+import org.joda.time.LocalTime;
+
+class Talk
+{
+  static final Duration SHORT_TALK_DURATION = Duration.standardMinutes(5L);
+  String talkName;
+  Duration talkDuration;
+  private LocalTime startsOn;
+  LocalTime endsOn;
+
+  static Talk normalTalk(String talkName, Duration talkDuration, LocalTime startsOn)
+  {
+    return new Talk(talkName, talkDuration, startsOn);
+  }
+
+  static Talk shortTalk(String talkName, LocalTime startsOn)
+  {
+    return new Talk(talkName, SHORT_TALK_DURATION, startsOn);
+  }
+
+  Talk(String talkName, Duration talkDuration, LocalTime startsOn)
+  {
+    this.talkName = talkName;
+    this.talkDuration = talkDuration;
+    this.startsOn = startsOn;
+    this.endsOn = startsOn.plus(talkDuration.toStandardSeconds());
+  }
+
+  public boolean isTalkOverlapping(LocalTime proposedStartTimeOfTalk, LocalTime proposedEndsTimeOfTalk)
+  {
+    return ((proposedStartTimeOfTalk.isAfter(this.startsOn)) && (proposedStartTimeOfTalk.isBefore(this.endsOn))) || ((proposedStartTimeOfTalk.isBefore(this.startsOn)) && (proposedEndsTimeOfTalk.isAfter(this.startsOn)));
+  }
+
+  public boolean isShortTalk()
+  {
+    return this.talkDuration.equals(SHORT_TALK_DURATION);
+  }
+
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if ((o == null) || (getClass() != o.getClass())) {
+      return false;
+    }
+    Talk talk = (Talk)o;
+    if (this.talkName != null ? !this.talkName.equals(talk.talkName) : talk.talkName != null) {
+      return false;
+    }
+    return true;
+  }
+
+  public int hashCode()
+  {
+    return this.talkName != null ? this.talkName.hashCode() : 0;
+  }
+
+  static boolean isValidNetworkingEventStartTime(LocalTime startsOn)
+  {
+    return (startsOn.isAfter(LocalTime.parse("T16:00:00"))) && (startsOn.isBefore(LocalTime.parse("T17:00:00")));
+  }
+}
 
 
 /* Location:           F:\codebases\codekata\confmangt\target\classes\
---
+++ src/test/java/com/conf/ConferenceTest.java
@@ -0,0 +1,203 @@
+package com.conf;
+
+import org.hamcrest.CoreMatchers;
+import org.joda.time.Duration;
+import org.joda.time.LocalTime;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ConferenceTest {
+    Conference conferenceWithThreeTracksCapacity;
+
+    @Before
+    public void setUp()
+            throws Exception {
+        this.conferenceWithThreeTracksCapacity = Conference.Plan("Performance Conference", 3);
+    }
+
+    @Test
+    public void whenConferenceWithThreeTracksIsPlanned_itShouldCreateThreeSeparateTracksWithinConference() {
+        Assert.assertThat(this.conferenceWithThreeTracksCapacity.noOfTracks(), is(Integer.valueOf(3)));
+    }
+
+    @Test
+    public void givenAConferenceWithThreeTracks_WhenFirstTrackIsAdded_itShouldReturnTrue() {
+        Boolean isTrackAdded = Boolean.valueOf(this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning"));
+        Assert.assertThat(isTrackAdded, is(Boolean.TRUE));
+    }
+
+    @Test
+    public void givenAConferenceWithOneTrack_WhenSecondTrackIsAdded_itShouldReturnFalse() {
+        Conference conferenceWithOneTrack = Conference.Plan("SingleTrackConf", 1);
+        Boolean isTrackAdded = Boolean.valueOf(conferenceWithOneTrack.addTrack("First Track"));
+        isTrackAdded = Boolean.valueOf(conferenceWithOneTrack.addTrack("Second Track"));
+        Assert.assertThat(isTrackAdded, is(Boolean.FALSE));
+    }
+
+    @Test(expected = Track.InValidTalkTimingsException.class)
+    public void whenTalkIsScheduledBeforeNineAM_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T8:50:00"));
+    }
+
+    @Test(expected = Track.InValidTalkTimingsException.class)
+    public void whenTalkIsScheduledBetweenOneAndTwoPM_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T1:50:00"));
+    }
+
+    @Test(expected = Track.InValidTalkTimingsException.class)
+    public void whenTalkIsScheduledAfterFivePM_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T17:50:00"));
+    }
+
+    @Test(expected = Track.InValidTalkTimingsException.class)
+    public void givenATalkIsAddedToConference_whenEndTimeExceedsFivePM_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T16:20:00"));
+    }
+
+    @Test(expected = Track.InValidTalkTimingsException.class)
+    public void givenATalkIsAddedToConference_whenEndTimeExceedsOnePM_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T12:50:00"));
+    }
+
+    @Test
+    public void givenConferenceWithTrack_whenTalkIsScheduled_itShouldFindTalkInTheTrack() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "Adv Tuning";
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T13:50:00"));
+
+        Talk talk = this.conferenceWithThreeTracksCapacity.findTrack("JVM Tunning").findTalk("Adv Tuning");
+        Assert.assertThat(talk.talkName, is("Adv Tuning"));
+        Assert.assertThat(talk.endsOn, is(equalTo(LocalTime.parse("T14:30:00"))));
+    }
+
+    @Test
+    public void givenConferenceWithTrack_whenShortTalkIsScheduled_itShouldFinishInFiveMinutes() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "Adv Tuning";
+        this.conferenceWithThreeTracksCapacity.scheduleShortTalk("JVM Tunning", "Adv Tuning", LocalTime.parse("T13:50:00"));
+
+        Talk talk = this.conferenceWithThreeTracksCapacity.findTrack("JVM Tunning").findTalk("Adv Tuning");
+        Assert.assertThat(talk.talkName, is("Adv Tuning"));
+        Assert.assertThat(talk.endsOn, is(equalTo(LocalTime.parse("T13:55:00"))));
+    }
+
+    @Test
+    public void givenAScheduledTalk_whenItIsCancelled_itShouldReturnTrue() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "Adv Tuning";
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(40L), LocalTime.parse("T13:50:00"));
+
+        boolean isCancelled = this.conferenceWithThreeTracksCapacity.cancelTalk("JVM Tunning", "Adv Tuning");
+        Assert.assertThat(Boolean.valueOf(isCancelled), is(Boolean.TRUE));
+    }
+
+    @Test
+    public void givenAInvalidTalk_whenItIsCancelled_itShouldReturnFalse() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "InvalidTalk";
+
+        boolean isCancelled = this.conferenceWithThreeTracksCapacity.cancelTalk("JVM Tunning", "InvalidTalk");
+        Assert.assertThat(Boolean.valueOf(isCancelled), is(Boolean.FALSE));
+    }
+
+    @Test
+    public void givenANetworkingEvent_itShouldStartBetween4And5PM() {
+    }
+
+    @Test(expected = Track.TalkTimingsOverlapException.class)
+    public void givenAScheduledTalk_whenANewTalkIsScheduledInBetweenAExistingTalk_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(30L), LocalTime.parse("T13:00:01"));
+
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Clash Talk", Duration.standardMinutes(30L), LocalTime.parse("T13:15:00"));
+    }
+
+    @Test(expected = Track.TalkTimingsOverlapException.class)
+    public void givenAScheduledTalk_whenANewTalkIsXXXXXX_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(30L), LocalTime.parse("T14:00:00"));
+
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Clash Talk", Duration.standardMinutes(30L), LocalTime.parse("T13:45:00"));
+    }
+
+    @Test(expected = Track.InValidNetworkingEventTimingException.class)
+    public void whenNetworkingEventSchduledAtFourPM_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        this.conferenceWithThreeTracksCapacity.scheduleNetworkingEvent("JVM Tunning", LocalTime.parse("T16:00:00"));
+    }
+
+    @Test(expected = Track.TalkTimingsOverlapException.class)
+    public void givenConferenceWithTalks_whenNetworkingEventOverlapsWithScheduledTalk_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(60L), LocalTime.parse("T15:30:00"));
+
+        this.conferenceWithThreeTracksCapacity.scheduleNetworkingEvent("JVM Tunning", LocalTime.parse("T16:15:00"));
+    }
+
+    @Test
+    public void givenAConferenceWithTalks_whenATalkIsRescheduled_itShouldChangeTheTalkStartTime() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "Adv Tuning";
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(59L), LocalTime.parse("T15:30:00"));
+
+        this.conferenceWithThreeTracksCapacity.rescheduleTalk("JVM Tunning", "Adv Tuning", LocalTime.parse("T16:00:00"));
+        Talk rescheduledTalk = this.conferenceWithThreeTracksCapacity.findTrack("JVM Tunning").findTalk("Adv Tuning");
+
+        Assert.assertThat(rescheduledTalk.endsOn, is(equalTo(LocalTime.parse("T16:59:00"))));
+    }
+
+    @Test
+    public void givenAConferenceWithTalks_whenAShortTalkIsRescheduled_itShouldChangeTheTalkStartTime() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "Adv Tuning";
+        this.conferenceWithThreeTracksCapacity.scheduleShortTalk("JVM Tunning", "Adv Tuning", LocalTime.parse("T15:30:00"));
+
+        this.conferenceWithThreeTracksCapacity.rescheduleTalk("JVM Tunning", "Adv Tuning", LocalTime.parse("T16:00:00"));
+        Talk rescheduledTalk = this.conferenceWithThreeTracksCapacity.findTrack("JVM Tunning").findTalk("Adv Tuning");
+
+        Assert.assertThat(rescheduledTalk.endsOn, is(equalTo(LocalTime.parse("T16:05:00"))));
+    }
+
+    @Test(expected = Track.TalkTimingsOverlapException.class)
+    public void givenATalkIsRescheduled_whenItOverlapsWithExistingTalk_itShouldThrowException() {
+        String trackName = "JVM Tunning";
+        this.conferenceWithThreeTracksCapacity.addTrack("JVM Tunning");
+        String talkName = "Adv Tuning";
+        String talkName1 = "talkName1 Adv Tuning";
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "Adv Tuning", Duration.standardMinutes(30L), LocalTime.parse("T15:30:00"));
+        this.conferenceWithThreeTracksCapacity.scheduleTalk("JVM Tunning", "talkName1 Adv Tuning", Duration.standardMinutes(30L), LocalTime.parse("T16:00:00"));
+
+        this.conferenceWithThreeTracksCapacity.rescheduleTalk("JVM Tunning", "Adv Tuning", LocalTime.parse("T16:05:00"));
+    }
+}
+
+
+/* Location:           F:\codebases\codekata\confmangt\target\test-classes\
+ * Qualified Name:     com.conf.ConferenceTest
+ * JD-Core Version:    0.7.0.1
+ */
